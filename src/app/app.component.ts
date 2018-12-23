94c15dd02524586b684bb64b98e3f505f834b99f
Jialiang Zhong <jialiang1984@gmail.com>
2018-12-23 18:31:24 -0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+declare var PIXI: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'pixi-test';
+export class AppComponent implements OnInit{
+  public app: any;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    this.app = new PIXI.Application(window.innerWidth / 2, window.innerHeight, { backgroundColor: 0x000000 });
+    let leftPanel = document.getElementById('left-panel').appendChild(this.app.view);
+    let basicText = new PIXI.Text('Basic text in pixi');
+    basicText.x = 30;
+    basicText.y = 90;
+
+    this.app.stage.addChild(basicText);
+  }
 }
