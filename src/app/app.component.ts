4fe69e38a9be0904f11960a0560758bed029d8f5
Jialiang Zhong <jialiang1984@gmail.com>
2018-12-25 14:27:59 -0500

    Testing file imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import myFunction from 'src/app/javascipt_stuff/myFunction.js'; // Importing
+
 declare var PIXI: any;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -16,6 +19,7 @@ export class AppComponent implements OnInit {
     this.app = new PIXI.Application(window.innerWidth / 2, window.innerHeight, { backgroundColor: 0xFFFFFF });
     const pixiWindow = document.getElementById('pixi-window').appendChild(this.app.view);
     const basicText = new PIXI.Text('Basic text in pixi');
+    console.log(myFunction(1, 2));
     basicText.x = 30;
     basicText.y = 90;
 
---
+++ src/app/javascipt_stuff/myFunction.js
@@ -0,0 +1,3 @@
+export default function myFunction(p1, p2) {
+    return p1 * p2; // The function returns the product of p1 and p2
+}
