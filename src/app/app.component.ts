bdccd4541c9184b6513c90c9e280bb1fd0e5b4dd
Jialiang Zhong <jialiang1984@gmail.com>
2019-10-22 11:52:10 -0400

    Draggable cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,8 @@ import { EventManager } from 'src/app/boardgame/EventManager.js'; // Importing
 import { Piece } from 'src/app/boardgame/Piece.js'; // Importing
 
 declare var PIXI: any;
+const renderer = PIXI.autoDetectRenderer(800, 600);
+document.body.appendChild(renderer.view);
 
 @Component({
   selector: 'app-root',
@@ -19,6 +21,7 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.app = new PIXI.Application(window.innerWidth / 2, window.innerHeight, { backgroundColor: 0xFFFFFF });
+
     const pixiWindow = document.getElementById('pixi-window').appendChild(this.app.view);
     const test_class = new EventManager.EventDisplayText(this.app, 'hello');
     const CARD_WIDTH = 81;
@@ -32,6 +35,27 @@ export class AppComponent implements OnInit {
         const texture = new PIXI.Texture(PIXI.Texture.fromImage('assets/sprites/cards_sprite.gif'),
           new PIXI.Rectangle(row * CARD_WIDTH, col * CARD_HEIGHT, CARD_WIDTH, CARD_HEIGHT));
         const sprite = new PIXI.Sprite(texture);
+
+        sprite.interactive = true;
+
+        // this button mode will mean the hand cursor appears when you roll over the bunny with your mouse
+        sprite.buttonMode = true;
+        // center the bunny's anchor point
+        sprite.anchor.set(0.5);
+        // setup events
+        sprite
+            // events for drag start
+            .on('mousedown', onDragStart)
+            .on('touchstart', onDragStart)
+            // events for drag end
+            .on('mouseup', onDragEnd)
+            .on('mouseupoutside', onDragEnd)
+            .on('touchend', onDragEnd)
+            .on('touchendoutside', onDragEnd)
+            // events for drag move
+            .on('mousemove', onDragMove)
+            .on('touchmove', onDragMove);
+
         card.set_sprite(sprite);
         deck.push(card);
       }
@@ -43,11 +67,37 @@ export class AppComponent implements OnInit {
         display_height += CARD_HEIGHT;
         display_width = 0;
       }
-      deck[i].set_position(i * (display_width + 2), display_height);
+      deck[i].set_position(i * (display_width + 6), display_height);
       deck[i].render();
     }
 
     test_class.set_position(190, 90);
     test_class.display_text();
   }
+}
+
+function onDragStart(event) {
+    // store a reference to the data
+    // the reason for this is because of multitouch
+    // we want to track the movement of this particular touch
+    this.data = event.data;
+    this.alpha = 0.5;
+    this.dragging = true;
+}
+
+function onDragEnd() {
+    this.alpha = 1;
+
+    this.dragging = false;
+
+    // set the interaction data to null
+    this.data = null;
+}
+
+function onDragMove() {
+    if (this.dragging) {
+        const newPosition = this.data.getLocalPosition(this.parent);
+        this.position.x = newPosition.x;
+        this.position.y = newPosition.y;
+    }
 }
